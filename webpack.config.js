05168255df564daae6e70fef37f07f37a08983bf
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2019-05-10 08:44:12 +0300

    updated linters, added NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ let CopyWebpackPlugin = require('copy-webpack-plugin');
 const js = require('./webpack/js');
 const css = require('./webpack/css');
 const favicon = require('./webpack/favicon');
+const webworker = require('./webpack/webworker');
+const nodeEnv = require('./webpack/node.env');
 
 const sourceMap = require('./webpack/sourceMap');
 const devserver = require('./webpack/devserver');
@@ -14,7 +16,7 @@ const lintCSS = require('./webpack/sass.lint');
 
 const PATHS = {
   source: path.join(__dirname, 'src'),
-  build: path.join(__dirname, 'build'),
+  build: path.join(__dirname, 'build')
 };
 
 const common = merge([
@@ -33,29 +35,34 @@ const common = merge([
 
       new CopyWebpackPlugin([
         { from: 'src/img/', to: 'img/', force: true },
-        { from: 'src/fonts/', to: 'fonts/', force: true }
+        { from: 'src/fonts/', to: 'fonts/', force: true },
+        { from: 'src/robots.txt', to: '', force: true }
       ], {})
     ]
   },
   js(),
   css(),
   favicon(),
+  webworker()
 ]);
 
 module.exports = function(env, argv) {
+  console.log(argv.mode);
   if (argv.mode === 'development') {
     return merge([
       common,
       devserver(),
       sourceMap(),
       lintJS({ paths: PATHS.source }),
-      lintCSS()
+      lintCSS(),
+      nodeEnv(argv.mode)
     ]);
   }
 
   if (argv.mode === 'production') {
     return merge([
-      common
+      common,
+      nodeEnv(argv.mode)
     ]);
   }
 };
